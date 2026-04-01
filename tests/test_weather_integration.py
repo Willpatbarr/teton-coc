bc4ac83100c8a47735bee1f78e59fe5d5d3208d6
Willpatbarr <145700970+Willpatbarr@users.noreply.github.com>
2026-04-01 12:39:08 -0600

    Test 12.2 Workshop Job 3

--- tests/test_weather_integration.py
+++ tests/test_weather_integration.py
@@ -2,7 +2,6 @@ import os
 import pytest
 from weather.client import get_weather_temperature
 # Skip this test if the WEATHER_API_KEY is not present in the environment
-# random comment to update the title of the workflow run
 pytestmark = pytest.mark.skipif(
     "WEATHER_API_KEY" not in os.environ,
     reason="WEATHER_API_KEY not set"
